8b17cb9f86a0b4be9f5a678d8321f05f9e648098
Abhishek Gaur <abhishekgaur797@gmail.com>
2026-05-07 10:38:47 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& nums) {
+        bool row[9][9] = {false};
+        bool col[9][9] = {false};
+        bool box[9][9] = {false};
+
+        for(int i = 0 ; i < 9 ; i++){
+            for(int j = 0 ; j < 9 ; j++){
+                if(nums[i][j] == '.') continue;
+
+                int d = nums[i][j]-'1';
+                int b = (i/3)*3+(j/3);
+
+                if(row[i][d] || col[j][d] || box[b][d]){
+                    return false;
+                }
+                row[i][d] = true;
+                col[j][d] = true;
+                box[b][d] = true;
+
+            }
+        }
+        return true;
+    }
+};
